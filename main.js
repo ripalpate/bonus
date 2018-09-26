22fb0d33575a57ea68d11d9fd1c50e3cad680c32
Ripal Patel <ripal212007@gmail.com>
2018-09-25 19:27:32 -0500

    added function to display dotted border around person's card

--- main.js
+++ main.js
@@ -51,15 +51,15 @@ const createCards =() => {
     let newCards = '';
     for(let i = 0; i < famous.length; i++) {
         newCards += `<div class='person_cards' id='person_cards_${i}' `;
-        newCards += `<header class='person_header'>`;
+        newCards += `<header id='person_header'>`;
         newCards += `<h3>${famous[i].name}</h3>`;
         newCards += `<h4>${famous[i].title}</h4>`;
         newCards += `</header>`;
-        newCards += `<section class='person_bio'>`;
+        newCards += `<section id='person_bio'>`;
         newCards += `<p>${famous[i].bio}</p>`;
         newCards += `<img src="${famous[i].image}" width="200px" height="200px"></img>`;
         newCards += `</section>`;
-        newCards += `<footer class="person_footer">`;
+        newCards += `<footer id="person_footer">`;
         newCards += `<p>${famous[i].lifespan.birth}</p>`;
         newCards += `<p>${famous[i].lifespan.death}</p>`;
         newCards +=`</footer>`;
@@ -69,25 +69,17 @@ const createCards =() => {
     }
 }
 createCards();
-
-const personsContainer = document.getElementById('person_container');
-    // collection of children of Person Container Div
-    let childrens = personsContainer.children;
-    let element = childrens.item(0);
-    let element1 = childrens.item(1); 
-    let element2 = childrens.item(2);
-    let element3 = childrens.item(3);
-        if(element){
-            element.addEventListener('click',()=>{
-            element.style.border="1px dotted black";});
-        } if (element1){
-            element1.addEventListener('click',()=>{
-            element1.style.border="1px dotted red";});
-        } if (element2){
-            element2.addEventListener('click',()=>{
-            element2.style.border="1px dotted blue";});
-        } if (element3){
-            element3.addEventListener('click',()=>{
-            element3.style.border="1px dotted brown";});
-        }
-            
+// function to put border around each person's card
+const childrenString = () => {
+    const personsContainer = document.getElementById('person_container');
+        // collection of children of Person Container Div
+        let childrens = personsContainer.children;
+        for(let i =0; i<childrens.length; i++) {
+            const element = childrens[i];
+            element.addEventListener('click', (e)=>{
+                const element = childrens[i];
+                element.classList.toggle('border');
+            })  
+    }
+};
+childrenString();
